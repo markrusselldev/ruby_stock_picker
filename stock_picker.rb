d037d2aa6dccb6a05e745ca0209fe8bae6152642
markrusselldev <mark@markrussell.dev>
2023-01-07 14:24:47 -0700

    feat: complete stock_picker function
    flatten and pop output to desired output array

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,8 +6,9 @@ def stock_picker(prices)
       low_index < high_index && results[high_price - low_price] = [low_index, high_index]
     end
   end
-  
-  return results
+  # we need to get our highest profit and return only the desired value
+  output = results.max_by {|k, v| k}.flatten.pop(2)
+  return output
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
