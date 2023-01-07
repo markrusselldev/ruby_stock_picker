3a83d5522746c3710354d0846bb97364440b36b6
markrusselldev <mark@markrussell.dev>
2023-01-07 14:38:03 -0700

    style: improve ruby style using ruby style guide
    add correct spaces around brackets
    change index to idx to shorten long line in nested map

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,13 @@
 def stock_picker(prices)
   results = {}
-  prices.map.with_index do |low_price, low_index|
-    prices.map.with_index do |high_price, high_index|
+  prices.map.with_index do |low_price, low_idx|
+    prices.map.with_index do |high_price, high_idx|
       # we want to map these left to right only
-      low_index < high_index && results[high_price - low_price] = [low_index, high_index]
+      low_idx < high_idx && results[high_price - low_price] = [low_idx, high_idx]
     end
   end
-  # we need to get our highest profit and return only the desired value
-  output = results.max_by {|k, v| k}.flatten.pop(2)
+  # we need to get our highest profit and return only the desired values array
+  output = results.max_by { |k, v| k }.flatten.pop(2)
   return output
 end
 
